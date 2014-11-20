ed5d044cc41b374997147a093a3be321be417b85
Abhinav Kharbanda <abhinav9325@gmail.com>
2014-11-20 16:51:57 +0530

    Final Push

--- Almost Sorted.py
+++ Almost Sorted.py
@@ -1,21 +1,30 @@
+# Solution for the problem https://www.hackerrank.com/challenges/almost-sorted
 ninput= int(raw_input());
 num_array=raw_input().split();
+num_array=map(int,num_array)
 count=0;
 pos=0;
 i=0;
 flag=1;
 
 def check():
-	if(all(int(num_array[i]) <= int(num_array[i+1]) for i in xrange(len(num_array)-1))):
-		return True;
-	else:
-		return False;
-
+	return (num_array==sorted(num_array,key=int))
+def swap(a,b):
+    temp=num_array[a];
+    num_array[a]=num_array[b];
+    num_array[b]=temp
+    if(check()):
+        print "yes";
+        print ("swap %s %s" % (a+1,b+1));
+        return True;
+    else:
+        return False;
+    
+    
 if(check()):
 	print "yes";
 else:
 	while(i<len(num_array)-1):
-
 		if(num_array[i]>num_array[i+1]):
 			j=i;
 			pos=i;
@@ -24,26 +33,22 @@ else:
 				count=count+1;
 				j=j+1;
 			if(count==1):
-				temp=num_array[pos];
-				num_array[pos]=num_array[pos+1];
-				num_array[pos+1]=temp;
-				if(check()):
-					print "yes";
-					print ("swap %s %s" % (pos+1,pos+2));
-				else:
-					flag=0;
-				break;
-				
+                            temp_l=sorted(num_array)
+                            if(swap(i,num_array.index(temp_l[i]))):
+                                break
+                            else:
+                                flag=0
+                                break
 			elif(count>1):
-				num_array[pos:pos+count]=num_array[-(len(num_array)-pos+count):-(len(num_array)-pos)];
+                                num_array[pos:pos+count+1]=num_array[pos+count:pos-1:-1] 
 				if(check()):
 					print "yes";
 					print("reverse %s %s" % (pos+1,pos+count+1));
+					break;
 				else:
 					flag=0;
-				break;
-			
+					break;                    
+
 		i=i+1;
 	if(flag==0):
 		print "no";
-
